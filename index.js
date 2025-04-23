4ad89c0f8498d9413e3035e9ca9e8a96fe0f0dc7
anas-007 <skanaso7@gmail.com>
2025-04-23 10:06:12 +0400

    Increase payload size limit

--- index.js
+++ index.js
@@ -8,18 +8,28 @@ const app = express();
 const PORT = 3000;
 
 // Middleware
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 //app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MySQL connection
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '', // enter your MySQL password here
+//   database: 'formapp'
+// });
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', // enter your MySQL password here
-  database: 'formapp'
-});
-
+    host: '127.0.0.1', // force IPv4 to avoid ::1 issues
+    user: 'root',
+    password: '', // update if needed
+    database: 'formapp'
+  });
+  
 db.connect(err => {
   if (err) {
     console.log("Database connection failed:", err);
